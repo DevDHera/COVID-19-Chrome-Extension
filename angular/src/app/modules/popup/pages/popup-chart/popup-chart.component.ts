2b19fdcb02d841e35256077cae662fd14af51ba7
Devin <devdhera@gmail.com>
2020-04-25 16:13:07 +0530

    Completed Charts View

--- angular/src/app/modules/popup/pages/popup-chart/popup-chart.component.ts
+++ angular/src/app/modules/popup/pages/popup-chart/popup-chart.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Papa } from 'ngx-papaparse';
+import { ChartDataSets, ChartOptions } from 'chart.js';
+import { Label, Color } from 'ng2-charts';
+
+import { ApiService } from '../../../../services/api.service';
 
 @Component({
   selector: 'app-popup-chart',
@@ -6,7 +11,136 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./popup-chart.component.scss'],
 })
 export class PopupChartComponent implements OnInit {
-  constructor() {}
+  @Output() loadCharts = new EventEmitter();
 
-  ngOnInit() {}
+  isLoading = true;
+  isError = false;
+  datesDataSet: string[];
+  totalCasesDataSet: string[];
+  deathsDataSet: string[];
+  recoveriesDataSet: string[];
+  public lineChartData: ChartDataSets[] = [];
+  public lineChartLabels: Label[] = [];
+  public lineChartOptions: ChartOptions & { annotation: any } = {
+    responsive: true,
+    scales: {
+      xAxes: [{}],
+      yAxes: [
+        {
+          id: 'y-axis-0',
+          position: 'left',
+        },
+      ],
+    },
+    annotation: {
+      annotations: [
+        {
+          type: 'line',
+          mode: 'vertical',
+          scaleID: 'x-axis-0',
+          value: 'March',
+          borderColor: 'orange',
+          borderWidth: 2,
+          label: {
+            enabled: true,
+            fontColor: 'orange',
+            content: 'LineAnno',
+          },
+        },
+      ],
+    },
+  };
+  public lineChartColors: Color[] = [
+    {
+      // grey
+      backgroundColor: 'rgba(148,159,177,0.2)',
+      borderColor: 'rgba(148,159,177,1)',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)',
+    },
+    {
+      // green
+      backgroundColor: 'rgba(80,205,138,0.2)',
+      borderColor: 'rgba(80,205,138,1)',
+      pointBackgroundColor: 'rgba(80,205,138,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(80,205,138,1)',
+    },
+    {
+      // red
+      backgroundColor: 'rgba(255,0,0,0.3)',
+      borderColor: 'red',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)',
+    },
+  ];
+  public lineChartLegend = true;
+  public lineChartType = 'line';
+
+  constructor(private apiService: ApiService, private papa: Papa) {}
+
+  ngOnInit() {
+    this.apiService.getCsvStats().subscribe(
+      (data) => {
+        this.papa.parse(data, {
+          header: false,
+          delimiter: ',',
+          skipEmptyLines: true,
+          complete: (result) => {
+            [
+              this.datesDataSet,
+              this.totalCasesDataSet,
+              this.deathsDataSet,
+              this.recoveriesDataSet,
+            ] = result.data;
+
+            this.populateChartData();
+          },
+        });
+      },
+      (error) => {
+        this.isLoading = false;
+        this.isError = true;
+      }
+    );
+  }
+
+  populateChartData() {
+    const totalCases = [];
+    const recoveries = [];
+    const deaths = [];
+    let i;
+    for (i = 0; i <= this.datesDataSet.length; i += 7) {
+      this.lineChartLabels.push(this.datesDataSet[i]);
+      totalCases.push(this.totalCasesDataSet[i]);
+      recoveries.push(this.recoveriesDataSet[i]);
+      deaths.push(this.deathsDataSet[i]);
+    }
+
+    // Add Current Date
+    if (i !== this.datesDataSet.length) {
+      this.lineChartLabels.push(
+        this.datesDataSet[this.datesDataSet.length - 1]
+      );
+      totalCases.push(this.totalCasesDataSet[this.datesDataSet.length - 1]);
+      recoveries.push(this.recoveriesDataSet[this.datesDataSet.length - 1]);
+      deaths.push(this.deathsDataSet[this.datesDataSet.length - 1]);
+    }
+
+    this.lineChartData.push(
+      { data: totalCases, label: 'Total Cases' },
+      { data: recoveries, label: 'Recoveries' },
+      { data: deaths, label: 'Deats' }
+    );
+    this.isLoading = false;
+  }
+
+  loadCardsView() {
+    this.loadCharts.emit(false);
+  }
 }
--- angular/src/app/modules/popup/popup.module.ts
+++ angular/src/app/modules/popup/popup.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { CountUpModule } from 'ngx-countup';
+import { ChartsModule } from 'ng2-charts';
 
 import { PopupComponent } from './pages/popup/popup.component';
 import { PopupChartComponent } from './pages/popup-chart/popup-chart.component';
@@ -9,6 +10,6 @@ import { PopupRoutingModule } from './popup-routing.module';
 
 @NgModule({
   declarations: [PopupComponent, PopupCardsComponent, PopupChartComponent],
-  imports: [CommonModule, PopupRoutingModule, CountUpModule],
+  imports: [CommonModule, PopupRoutingModule, CountUpModule, ChartsModule],
 })
 export class PopupModule {}
