bf6f736c35e681e629dbf71e927a0ac4ad8b9b06
Devin <devdhera@gmail.com>
2020-04-25 12:30:44 +0530

    Setup animated counters

--- angular/src/app/modules/popup/popup.module.ts
+++ angular/src/app/modules/popup/popup.module.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { CountUpModule } from 'ngx-countup';
+
 import { PopupComponent } from './pages/popup/popup.component';
 import { PopupRoutingModule } from './popup-routing.module';
 
 @NgModule({
   declarations: [PopupComponent],
-  imports: [CommonModule, PopupRoutingModule]
+  imports: [CommonModule, PopupRoutingModule, CountUpModule],
 })
 export class PopupModule {}
