8ec5f40dc81ac162667437ddf8e37c79ee580727
Devin <devdhera@gmail.com>
2020-04-25 12:30:16 +0530

    Changed base setup

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './modules/shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, SharedModule],
   bootstrap: [AppComponent],
   exports: [HttpClientModule],
 })
