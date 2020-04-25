c9d849fe1799d1b5a9da98cdfcc24379d9e874a8
Devin <devdhera@gmail.com>
2020-04-25 13:33:08 +0530

    Restructure app

--- angular/src/app/modules/popup/pages/popup/popup.component.ts
+++ angular/src/app/modules/popup/pages/popup/popup.component.ts
@@ -1,9 +1,4 @@
-import { Component, Inject, OnInit } from '@angular/core';
-import { bindCallback } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { TAB_ID } from '../../../../providers/tab-id.provider';
-import { ApiService } from '../../../../services/api.service';
-import { Response } from '../../../../models/response';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-popup',
@@ -11,46 +6,13 @@ import { Response } from '../../../../models/response';
   styleUrls: ['popup.component.scss'],
 })
 export class PopupComponent implements OnInit {
-  message: string;
-  stats: Response;
-  currentDate = new Date();
-  isLoading = false;
-  isError = false;
+  loadCharts = false;
 
-  constructor(
-    @Inject(TAB_ID) readonly tabId: number,
-    private apiService: ApiService
-  ) {}
+  constructor() {}
 
-  ngOnInit() {
-    this.getStats();
-  }
+  ngOnInit() {}
 
-  async onClick(): Promise<void> {
-    this.message = await bindCallback<string>(
-      chrome.tabs.sendMessage.bind(this, this.tabId, 'request')
-    )()
-      .pipe(
-        map((msg) =>
-          chrome.runtime.lastError
-            ? 'The current page is protected by the browser, goto: https://www.google.nl and try again.'
-            : msg
-        )
-      )
-      .toPromise();
-  }
-
-  getStats() {
-    this.isLoading = true;
-    this.apiService.getCurrentStats().subscribe(
-      (response: Response) => {
-        this.isLoading = false;
-        this.stats = response;
-      },
-      (error) => {
-        this.isLoading = false;
-        this.isError = true;
-      }
-    );
+  onLoadChartsView(flag: boolean) {
+    this.loadCharts = flag;
   }
 }
--- angular/src/app/modules/popup/popup.module.ts
+++ angular/src/app/modules/popup/popup.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { CountUpModule } from 'ngx-countup';
 
 import { PopupComponent } from './pages/popup/popup.component';
+import { PopupChartComponent } from './pages/popup-chart/popup-chart.component';
+import { PopupCardsComponent } from './pages/popup-cards/popup-cards.component';
 import { PopupRoutingModule } from './popup-routing.module';
 
 @NgModule({
-  declarations: [PopupComponent],
+  declarations: [PopupComponent, PopupCardsComponent, PopupChartComponent],
   imports: [CommonModule, PopupRoutingModule, CountUpModule],
 })
 export class PopupModule {}
