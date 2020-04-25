0441b096455010ce15743dc43a6ba4b4a8982618
Devin <devdhera@gmail.com>
2020-04-25 13:32:31 +0530

    Base setup for chart component

---
+++ angular/src/app/modules/popup/pages/popup-chart/popup-chart.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-popup-chart',
+  templateUrl: './popup-chart.component.html',
+  styleUrls: ['./popup-chart.component.scss'],
+})
+export class PopupChartComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
